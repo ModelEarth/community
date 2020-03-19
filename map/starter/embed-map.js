05b0cb3297df979c3a7aba4ba75c216fadee6030
datascape <heijns@dreamstudio.com>
2020-03-19 14:34:47 -0400

    Display second map

--- map/starter/embed-map.js
+++ map/starter/embed-map.js
@@ -7,6 +7,46 @@
 */
 
 var strVar="";
+strVar += "<!-- Top Map -->";
+strVar += "<style>";
+strVar += "  #allLegends {";
+strVar += "    color:#fff !important; ";
+strVar += "  }";
+strVar += "  #allLegends > .label {";
+strVar += "    color:#fff !important; ";
+strVar += "  }";
+strVar += "  #map {";
+strVar += "    height:400px;";
+strVar += "  }";
+strVar += "<\/style>";
+strVar += "<aside class=\"Intermodal_Ports\">";
+strVar += "group,lat,lon,value,address,city,state,zip";
+strVar += "Inman Rail Yard,33.795735,-84.439278,NS,1600 Marietta Rd NW,Atlanta,GA,30318";
+strVar += "Savannah Yard,32.069578,-81.142174,CSX,3000 Tremont Ave,Savannah,GA,31405";
+strVar += "James D. Mason ICTF,32.123934,-81.150914,NS,3 North Main St,Garden City,GA,31408";
+strVar += "Fairburn Industry Yard,33.554907,-84.595026,CSX,6700 McLarin Rd,Fairburn,GA,30213";
+strVar += "Whitaker Rail Yard,33.80944,-84.655451,NS,6000 Westside Rd,Austell,GA,30106";
+strVar += "Chatham ICTF,32.125311,-81.151331,CSX,2 Main St,Garden City,GA,31408";
+strVar += "Savannah,32.10035,-81.169963,NS,1 Charlie Gay Drive,Savannah,GA,31408";
+strVar += "Cordele Intermodal Center,31.966779,-83.755101,Heart GA,2902 East 13th Ave.,Cordele,GA,31010";
+strVar += "Port of Savannah Ocean Terminal,32.093702,-81.111692,\"Ro\/Ro, Breakbulk\",55 N Lathrop Ave,Savannah,GA,31415";
+strVar += "Port of Brunswick,31.125887,-81.541194,\"Ro\/Ro, Agri-Bulk\",157 Penniman Cir,Brunswick,GA,31523";
+strVar += "Port of Savannah,32.125,-81.151,Container,2 Main St,Savannah,GA,31407";
+strVar += "Port of Brunswick Mayor's Point Terminal,31.143373,-81.494753,Breakbulk,1100 Bay Street,Brunswick,GA,31520";
+strVar += "Port of Columbus,32.448734,-84.984038,Liquid Bulk,800 Lumpkin Blvd,Columbus,GA,31901";
+strVar += "Port of Bainbridge,30.902867,-84.606259,Liquid\/Dry Bulk,1321 Spring Creek Road,Bainbridge,GA,39817";
+strVar += "<\/aside>";
+strVar += "<div class=\"displayOnload\" style=\"display:none;position:relative;\">";
+strVar += "  <div id=\"map\"><\/div>";
+strVar += "  <div id=\"legendHolder\">";
+strVar += "    <div id=\"allLegends\"><\/div>";
+strVar += "  <\/div>";
+strVar += "<\/div>";
+strVar += "<\/section>";
+strVar += "<div style=\"clear:both\"><\/div>";
+strVar += "<!-- END Top Map -->";
+
+
 strVar += "<!-- Start HTML -->";
 strVar += "  <section id=\"data\" style=\"overflow:auto\">";
 strVar += "    <div class=\"content displayOnload\" style=\"display:none\">";
@@ -226,6 +266,42 @@ function dualmapLoaded() {
 	let root = "https://modelearth.github.io/community/";
 	//root = "https://model.earth/community/"; // CORS would need to be adjusted on server
 	//alert(root + "tools/map.csv");
+
+	if (1==1) {
+
+		// INTERMODAL PORTS
+	    dp = {
+	      selector: "aside.Intermodal_Ports",
+	      delimiter: ",",
+	      numColumns: ["lat","lon"],
+	      valueColumn: "value",
+	      scaleType: "scaleOrdinal",
+	      //scaleType: "scaleQuantile",
+	      //scaleType: "scaleThreshold",
+	    }
+	    //dp.name = dp.selector.split(".").pop(); // name: Intermodal_Ports
+	    dp.name = "Intermodal Ports"
+	    dp.data = readData(dp.selector, dp.delimiter, dp.numColumns, dp.valueColumn);
+	    //dp.color = '#0033ff'; // Alternative to scale
+	    dp.scale = getScale(dp.data, dp.scaleType, dp.valueColumn);
+
+	    dp.group = L.layerGroup();
+	    //dp.group2 = L.layerGroup();
+	    //dp.iconName = 'device_hub';
+	    dp.iconName = 'language';
+	    //dataParameters.push(dp);
+	    addIcons(dp);
+
+	    // Reactivate once colors differ
+	    //addLegend(dp.scale, dp.scaleType, dp.name);
+	    var map1 = populateMap('map', dp);
+	    //basemaps["Rail"].addTo(map1);
+
+	    layerControl['map'].addOverlay(baselayers["Rail"], "Railroads"); // Appends to existing layers
+
+
+	}
+
 	loadFromCSV('map2', root + "tools/map.csv", function(results) {
 		//alert("back");
 	//loadFromCSV('map2', "/community/tools/map.csv", function(results) {
