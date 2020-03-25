11933b49fcc470648ae024b4ba19790d14e06592
datascape <heijns@dreamstudio.com>
2020-03-25 10:31:09 -0400

    Update strVar

--- map/starter/embed-map.js
+++ map/starter/embed-map.js
@@ -51,35 +51,25 @@ strVar += "<!-- END Top Map -->";
 //let div = document.createElement("div");
 //div.innerHTML = strVar;
 //document.body.append(div);
-
 //strVar = "";
 
+// Replace afer updating:
+// Data Driven Decision Making
+// Smart & Sustainable Movement of <span style=\"white-space: nowrap\">Goods &amp; Services</span>
+
 strVar += "<!-- Start HTML -->";
+strVar += "  <a name=\"gomap\"><\/a>";
 strVar += "  <section id=\"data\" style=\"overflow:auto\">";
 strVar += "    <div class=\"content displayOnload\" style=\"display:none\">";
-strVar += "      <div id=\"flexwrapper\">";
-strVar += "        <div id=\"hublist\"><div style=\"padding-right:30px\">";
-strVar += "";
-strVar += "          <h1>Data Driven Decision&nbsp;Making<\/h1>";
-strVar += "          <h2 style=\"font-size: 18px; margin-bottom:0px; font-weight: 600\">Smart & Sustainable Movement of <span style=\"white-space: nowrap\">Goods & Services<\/span><\/h2><br>";
-strVar += "          ";
-strVar += "            <!--";
-strVar += "            <input type=\"button\" onclick=\"location.href='..\/..\/samples\/feed';\" ";
-strVar += "            value=\"Add your data feeds\" style=\"margin-top:20px; padding:10px\" \/>";
-strVar += "            <br><br>";
-strVar += "            -->";
-strVar += "";
-strVar += "          <div id=\"detaillist\"><\/div>";
-strVar += "          <div id=\"narrowlist\" style=\"display:none\"><\/div>";
-strVar += "        <\/div>";
-strVar += "";
-strVar += "        <a name=\"gomap\"><\/a>";
+strVar += "      <div id=\"flexwrapperX\">";
+strVar += "        ";
 strVar += "        <div id=\"mapHolder\">";
 strVar += "          <div class=\"mapHolderInner\">";
 strVar += "            <div class=\"mapHolderCard card\">";
 strVar += "              <div id=\"sidemapbar\">";
 strVar += "                <div style=\"position:absolute; left:0; padding:4px 4px 4px 10px\">";
-strVar += "                  Connected Communities";
+strVar += "                  ";
+strVar += "                  <div id=\"widgetTitle\">Connected Communities<\/div>";
 strVar += "                <\/div>";
 strVar += "              <\/div>";
 strVar += "";
@@ -88,14 +78,31 @@ strVar += "                <div id=\"map2\"><\/div>";
 strVar += "              <\/div>";
 strVar += "";
 strVar += "            <\/div>";
-strVar += "          <\/div><\/div>";
+strVar += "          <\/div>";
 strVar += "        <\/div>";
 strVar += "      ";
+strVar += "        <div id=\"hublist\">";
+strVar += "          <div id=\"hublist-padding\">";
+strVar += "            <h1>Data Driven Decision Making<\/h1>";
+strVar += "            <h2 style=\"font-size: 18px; margin-bottom:0px; font-weight: 600\">Smart & Sustainable Movement of <span style=\"white-space: nowrap\">Goods &amp; Services</span><\/h2><br>";
+strVar += "            ";
+strVar += "              <!--";
+strVar += "              <input type=\"button\" onclick=\"location.href='..\/..\/samples\/feed';\" ";
+strVar += "              value=\"Add your data feeds\" style=\"margin-top:20px; padding:10px\" \/>";
+strVar += "              <br><br>";
+strVar += "              -->";
+strVar += "";
+strVar += "            <div id=\"detaillist\"><\/div>";
+strVar += "            <div id=\"narrowlist\" style=\"display:none\"><\/div>";
+strVar += "          <\/div>";
+strVar += "        <\/div>";
+strVar += "";
 strVar += "      <\/div><!-- flexwrapper -->";
 strVar += "    <\/div>  ";
 strVar += "";
 strVar += "  <\/section>";
 strVar += "<!-- End HTML -->";
+
 
 
 strVar += "<style>";
