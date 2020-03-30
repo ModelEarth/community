1884e4a6868c9e5588735a68ffbabb225467f464
ndwarshuis <ndwar@yavin4.ch>
2020-03-30 15:22:40 -0400

    ADD push to google sheets

--- farmfresh/curbside/toGoogleSheets.py
+++ farmfresh/curbside/toGoogleSheets.py
@@ -14,15 +14,20 @@ restaurants = []
 
 # select all paragraphs containing links after the first level3 header
 for a in bs(page).select("h3 ~ p > a:nth-child(1)"):
-    restaurants.append((a["href"], "".join(str(i) for i in a.contents)))
+    restaurants.append(["".join(str(i) for i in a.contents), a["href"]])
 
-print(restaurants)
-
-# TODO doesn't work (error = Request had insufficient authentication scopes)
 # followed this guide: https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
 
-scope = ["https://spreadsheets.google.com/feeds"]
+# note this scope is different from the guide, this should give read/write
+# access to anything in the user's google drive
+scope = ["https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
 client = gspread.authorize(creds)
 
-sheet = client.open("Georgia-mapsfor.us").sheet1
+sheet = client.open("Georgia-mapsfor.us").get_worksheet(2)
+
+sheet.batch_update([{"range": "A1:B1", "values": [["Title", "link"]]}])
+
+rng = "A2:B" + str(len(restaurants) + 1)
+
+sheet.batch_update([{"range": rng, "values": restaurants}])
