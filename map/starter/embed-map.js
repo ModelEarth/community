1daf5c4d6fe17568cba7f1482ac7fa27637c44f1
datascape <heijns@dreamstudio.com>
2020-04-30 17:08:00 -0400

    Add map2 position updates to embed version

--- map/starter/embed-map.js
+++ map/starter/embed-map.js
@@ -6,6 +6,7 @@
 // Final version resides in embed-map.js
 */
 
+
 // Add \r to end of aside rows manually.
 var strVar="";
 strVar += "<!-- Top Map -->";
@@ -17,31 +18,15 @@ strVar += "  #allLegends > .label {";
 strVar += "    color:#fff !important; ";
 strVar += "  }";
 strVar += "  #map1 {";
-strVar += "    height:400px;";
+strVar += "    height:600px;";
 strVar += "  }";
 strVar += "  #hublist-padding {";
 strVar += "    padding-right: 30px;";
 strVar += "  }";
 strVar += "<\/style>";
-strVar += "<aside class=\"Intermodal_Ports\" style=\"display:none\">";
-strVar += "group,lat,lon,value,address,city,state,zip\r";
-strVar += "Inman Rail Yard,33.795735,-84.439278,NS,1600 Marietta Rd NW,Atlanta,GA,30318\r";
-strVar += "Savannah Yard,32.069578,-81.142174,CSX,3000 Tremont Ave,Savannah,GA,31405\r";
-strVar += "James D. Mason ICTF,32.123934,-81.150914,NS,3 North Main St,Garden City,GA,31408\r";
-strVar += "Fairburn Industry Yard,33.554907,-84.595026,CSX,6700 McLarin Rd,Fairburn,GA,30213\r";
-strVar += "Whitaker Rail Yard,33.80944,-84.655451,NS,6000 Westside Rd,Austell,GA,30106\r";
-strVar += "Chatham ICTF,32.125311,-81.151331,CSX,2 Main St,Garden City,GA,31408\r";
-strVar += "Savannah,32.10035,-81.169963,NS,1 Charlie Gay Drive,Savannah,GA,31408\r";
-strVar += "Cordele Intermodal Center,31.966779,-83.755101,Heart GA,2902 East 13th Ave.,Cordele,GA,31010\r";
-strVar += "Port of Savannah Ocean Terminal,32.093702,-81.111692,\"Ro\/Ro, Breakbulk\",55 N Lathrop Ave,Savannah,GA,31415\r";
-strVar += "Port of Brunswick,31.125887,-81.541194,\"Ro\/Ro, Agri-Bulk\",157 Penniman Cir,Brunswick,GA,31523\r";
-strVar += "Port of Savannah,32.125,-81.151,Container,2 Main St,Savannah,GA,31407\r";
-strVar += "Port of Brunswick Mayor's Point Terminal,31.143373,-81.494753,Breakbulk,1100 Bay Street,Brunswick,GA,31520\r";
-strVar += "Port of Columbus,32.448734,-84.984038,Liquid Bulk,800 Lumpkin Blvd,Columbus,GA,31901\r";
-strVar += "Port of Bainbridge,30.902867,-84.606259,Liquid\/Dry Bulk,1321 Spring Creek Road,Bainbridge,GA,39817\r";
-strVar += "<\/aside>";
+strVar += "";
 strVar += "<div class=\"displayOnload\" style=\"display:none;position:relative;\">";
-strVar += "  <div id=\"map1\" style=\"display:none\"><\/div>";
+strVar += "  <div style=\"display:block;position:relative\" id=\"map1\"><\/div>";
 strVar += "  <div id=\"legendHolder\">";
 strVar += "    <div id=\"allLegends\"><\/div>";
 strVar += "  <\/div>";
@@ -49,6 +34,7 @@ strVar += "<\/div>";
 strVar += "<\/section>";
 strVar += "<div style=\"clear:both\"><\/div>";
 strVar += "<!-- END Top Map -->";
+
 
 ////document.body.prepend(strVar); // This displayed as HTML tags
 //let div = document.createElement("div");
@@ -60,6 +46,7 @@ strVar += "<!-- END Top Map -->";
 // Data Driven Decision Making
 // Smart & Sustainable Movement of <span style=\"white-space: nowrap\">Goods &amp; Services</span>
 
+/*
 strVar += "<!-- Start HTML -->";
 strVar += "  <a name=\"gomap\"><\/a>";
 strVar += "  <section id=\"data\" style=\"overflow:auto\">";
@@ -105,15 +92,61 @@ strVar += "    <\/div>  ";
 strVar += "";
 strVar += "  <\/section>";
 strVar += "<!-- End HTML -->";
+*/
 
+
+strVar += "<!-- Start HTML -->";
+strVar += "  <a name=\"gomap\"><\/a>";
+strVar += "  <section id=\"data\" style=\"overflow:auto\">";
+strVar += "    <div class=\"content displayOnload\" style=\"display:none\">";
+strVar += "      <div id=\"flexwrapperX\">";
+strVar += "        ";
+strVar += "        <a name=\"gomap\"><\/a>";
+strVar += "        <div id=\"mapHolder\">";
+strVar += "          <div class=\"mapHolderInner\">";
+strVar += "            <div id=\"sidemapCard\" class=\"mapHolderCard card\">";
+strVar += "              <div id=\"sidemapbar\">";
+strVar += "                <div id=\"sidemapName\" style=\"position:absolute; left:0; padding:4px 4px 4px 10px\">";
+strVar += "                <\/div>";
+strVar += "              <\/div>";
+strVar += "              <div style=\"clear:both\">  ";
+strVar += "                <div id=\"map2\"><\/div>";
+strVar += "              <\/div>";
+strVar += "            <\/div>";
+strVar += "          <\/div>";
+strVar += "        <\/div>";
+strVar += "      ";
+strVar += "        <div id=\"hublist\">";
+strVar += "          <div id=\"hublist-padding\">";
+
+strVar += "            <h1>Data Driven Decision Making<\/h1>";
+strVar += "            <h2 style=\"font-size: 18px; margin-bottom:0px; font-weight: 600\">Smart & Sustainable Movement of <span style=\"white-space: nowrap\">Goods &amp; Services</span><\/h2><br>";
+
+strVar += "            <div id=\"detaillist\"><\/div>";
+strVar += "            <div id=\"narrowlist\" style=\"display:none\"><\/div>";
+strVar += "          <\/div>";
+strVar += "        <\/div>";
+strVar += "";
+strVar += "      <\/div><!-- flexwrapper -->";
+strVar += "    <\/div>  ";
+strVar += "";
+strVar += "  <\/section>";
+strVar += "<!-- End HTML -->";
 
 
 strVar += "<style>";
+strVar += "  #legendHolder {";
+strVar += "    min-width: 270px;";
+strVar += "  }";
+strVar += "<\/style>";
+
+strVar += "<style>";
+strVar += "#legendHolder {min-width: 270px;}";
 //strVar += "#mapHolder {display:none !important}";
 //strVar += "#hublist{width:100% !important}";
 strVar += "  <\/style>";
 
-document.write(strVar);
+document.write("<div style=\"display:block;position:relative\">" + strVar + "<\/div> ");
 
 // COMMON
 function loadScript(url, callback)
@@ -209,13 +242,26 @@ function loadParam(paramStr,hashStr) {
    }
    return params;
 }
+function mix(incoming, target) { // Combine two objects, priority to incoming. Delete blanks indicated by incoming.
+   for(var key in incoming) {
+     if (incoming.hasOwnProperty(key)) {
+        if (incoming[key] === null || incoming[key] === undefined || incoming[key] === '') {
+          delete target[key];
+        } else {
+          target[key] = incoming[key];
+        }
+     }
+   }
+   return target;
+}
 // END COMMON
 
 // UNIQUE TO PAGE
 function jsLoaded(root) {	
 	//loadScript(root + 'js/common/common.js');
 	if (location.host.indexOf('localhost') >= 0) {
-		loadScript(root + 'js/common/navigation.js');
+		// Causing map points to shift right, maybe due to later loading.
+		//loadScript(root + 'js/common/navigation.js');
 	}
 }
 function leafletLoaded(root) {	
@@ -263,44 +309,7 @@ function dualmapLoaded(param) {
 	//root = "https://model.earth/community/"; // CORS would need to be adjusted on server
 	//alert(root + "tools/map.csv");
 
-	if (param.data != "smart") {
-		document.getElementById("map1").style.display = "block";
-		// INTERMODAL PORTS
-	    let dp1 = {
-	      selector: "aside.Intermodal_Ports",
-	      delimiter: ",",
-	      numColumns: ["lat","lon"],
-	      valueColumn: "value",
-	      scaleType: "scaleOrdinal",
-	      //scaleType: "scaleQuantile",
-	      //scaleType: "scaleThreshold",
-	    }
-	    //dp1.name = dp.selector.split(".").pop(); // name: Intermodal_Ports
-	    dp1.dataTitle = "Intermodal Ports"
-	    dp1.markerType = "google";
-	    dp1.data = readData(dp1.selector, dp1.delimiter, dp1.numColumns, dp1.valueColumn);
-	    //dp1.color = '#0033ff'; // Alternative to scale
-	    dp1.scale = getScale(dp1.data, dp1.scaleType, dp1.valueColumn);
-
-	    // Atlanta
-    	dp1.latitude = 33.7490;
-    	dp1.longitude = -84.3880;
-
-	    dp1.group = L.layerGroup();
-	    //dp1.group2 = L.layerGroup();
-	    //dp1.iconName = 'device_hub';
-	    dp1.iconName = 'language';
-	    //dataParameters.push(dp1);
-	    dp1.nameColumn = "group";
-	    
-	    populateMap('map1', dp1, function(results) {
-		    loadScript(root + 'js/d3/d3-legend.js', function(results){
-		    	addLegend(dp1.scale, dp1.scaleType, dp1.name);
-		    });
-
-		    layerControl['map1'].addOverlay(baselayers["Rail"], "Railroads"); // Appends to existing layers
-	    });
-	}
+	
 
 	// MAP 2
 	let dp2 = {
@@ -311,9 +320,9 @@ function dualmapLoaded(param) {
     dp2.longitude = -83.4;
     
     dp2.markerType = "google";
-	loadFromCSV('map2', dp2, function(results) {
+	loadFromCSV('map1','map2', dp2, function(results) {
 		//alert("back");
-	//loadFromCSV('map2', "/community/tools/map.csv", function(results) {
+	//loadFromCSV('map1', 'map2', "/community/tools/map.csv", function(results) {
         // This function gets called by the geocode function on success
         //makeMap(results[0].geometry.location.lat(), results[0].geometry.location.lng());
         layerControl['map2'].addOverlay(baselayers["Rail"], "Railroads"); // Appends to existing layers
@@ -323,7 +332,7 @@ function dualmapLoaded(param) {
 
 
 /*
-loadFromCSV('map2', "/community/tools/map.csv", function(results) {
+loadFromCSV('map1', 'map2', "/community/tools/map.csv", function(results) {
     // This function gets called by the geocode function on success
     //makeMap(results[0].geometry.location.lat(), results[0].geometry.location.lng());
 
