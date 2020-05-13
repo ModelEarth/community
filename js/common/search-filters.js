15cf4356a1b20e3a0bd99d355dc3a69742c79bdf
datascape <heijns@dreamstudio.com>
2020-05-13 15:24:59 -0400

    Add close map

--- js/common/search-filters.js
+++ js/common/search-filters.js
@@ -259,6 +259,11 @@ $(document).ready(function () {
 	    $("#mapButton").show();
 	    event.stopPropagation();
    	});
+   	$("#hideSidemap").click(function() {
+	    $("#sidemapCard").hide();
+	    $("#detaillist > .detail").css("border","none");
+   	});
+
    	function clearFields() {
    		$(".eWidget").show();
    		hideNonListPanels();
