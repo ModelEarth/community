cc821444251a3ce174f4ec754a510884c42579fb
datascape <heijns@dreamstudio.com>
2020-05-21 07:27:15 -0400

    Change links to start

--- map/starter/embed-map.js
+++ map/starter/embed-map.js
@@ -77,7 +77,7 @@ strVar += "            <h1>Data Driven Decision Making<\/h1>";
 strVar += "            <h2 style=\"font-size: 18px; margin-bottom:0px; font-weight: 600\">Smart & Sustainable Movement of <span style=\"white-space: nowrap\">Goods &amp; Services</span><\/h2><br>";
 strVar += "            ";
 strVar += "              <!--";
-strVar += "              <input type=\"button\" onclick=\"location.href='..\/..\/samples\/feed';\" ";
+strVar += "              <input type=\"button\" onclick=\"location.href='..\/..\/start\/feed';\" ";
 strVar += "              value=\"Add your data feeds\" style=\"margin-top:20px; padding:10px\" \/>";
 strVar += "              <br><br>";
 strVar += "              -->";
