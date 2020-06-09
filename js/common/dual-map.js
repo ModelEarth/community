2b71b6721e615167569340035256ee49bb6679b7
datascape <heijns@dreamstudio.com>
2020-06-09 07:34:05 -0400

    Append with -2 appended

--- js/common/dual-map.js
+++ js/common/dual-map.js
@@ -145,8 +145,8 @@ function loadFromCSV(whichmap,whichmap2,dp,basemaps1,basemaps2,callback) {
        if (map.hasLayer(overlays1[dp.dataTitle])){
           overlays1[dp.dataTitle].remove();
        }
-       if (map2.hasLayer(overlays2[dp.dataTitle])){
-          overlays2[dp.dataTitle].remove();
+       if (map2.hasLayer(overlays2[dp.dataTitle + "-2"])){
+          overlays2[dp.dataTitle + "-2"].remove();
        }
 
        // Prevents dups of layer from appearing
@@ -154,14 +154,15 @@ function loadFromCSV(whichmap,whichmap2,dp,basemaps1,basemaps2,callback) {
        if (overlays1[dp.dataTitle]) {
           layerControl[whichmap].removeLayer(overlays1[dp.dataTitle]);
        }
-       if (overlays2[dp.dataTitle]) {
+       if (overlays2[dp.dataTitle + "-2"]) {
           // Not working, multiple checkboxes appear
-          layerControl[whichmap2].removeLayer(overlays2[dp.dataTitle]); // Not sure why, but 1 needs to be used instead of 2
+          layerControl[whichmap2].removeLayer(overlays2[dp.dataTitle + "-2"]); // Not sure why, but 1 needs to be used instead of 2
           //controlLayers.removeLayer(overlays2[dp.dataTitle]);
        }
 
-      overlays1[dp.dataTitle] = dp.group; // Allows for use of dp.dataTitle with removeLayer and addLayer
-      overlays2[dp.dataTitle] = dp.group2;
+      // Allows for use of dp.dataTitle with removeLayer and addLayer
+      overlays1[dp.dataTitle] = dp.group;
+      overlays2[dp.dataTitle + "-2"] = dp.group2;
 
       if (layerControl[whichmap] != undefined) {
         // Remove existing instance of layer
@@ -189,7 +190,7 @@ function loadFromCSV(whichmap,whichmap2,dp,basemaps1,basemaps2,callback) {
         layerControl[whichmap2] = L.control.layers(basemaps2, overlays2).addTo(map2); // Push multple layers
         basemaps2["OpenStreetMap"].addTo(map2); // Set the initial baselayer.
       } else {
-        layerControl[whichmap2].addOverlay(dp.group, dp.dataTitle); // Appends to existing layers
+        layerControl[whichmap2].addOverlay(dp.group2, dp.dataTitle + "-2"); // Appends to existing layers
       }
 
       if (dp.showLegend != false) {
@@ -205,8 +206,9 @@ function loadFromCSV(whichmap,whichmap2,dp,basemaps1,basemaps2,callback) {
         $("#widgetTitle").text(dp.dataTitle);
         dp = showList(dp,map); // Reduces list based on filters
         addIcons(dp,map,map2);
+        // These do not effect the display of layer checkboxes
         map.addLayer(overlays1[dp.dataTitle]);
-        map2.addLayer(overlays2[dp.dataTitle]);
+        map2.addLayer(overlays2[dp.dataTitle + "-2"]);
       }
       $("#activeLayer").text(dp.dataTitle); // Resides after showList
 
@@ -290,8 +292,8 @@ function populateMap(whichmap, dp, callback) { // From JSON within page
     overlays1[dp.dataTitle] = dp.group; // Allows for use of dp.name with removeLayer and addLayer
 
     // Adds checkbox, but unselects other map on page
-    overlays2[dp.dataTitle] = dp.group;
-
+    //overlays2[dp.dataTitle] = dp.group;
+    overlays2[dp.dataTitle  + "-2"] = dp.group2; //Haven't test switch to this
 
     /*
     if (layerControl[whichmap] == undefined) {
