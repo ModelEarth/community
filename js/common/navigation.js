faf1d36124dfb6796e7aa5548012093a061ee37f
datascape <heijns@dreamstudio.com>
2020-07-08 15:27:57 -0400

    Update navigation.js

--- js/common/navigation.js
+++ js/common/navigation.js
@@ -90,7 +90,7 @@ $(document).ready(function(){
 		 		$('.georgia').css('display', 'inline');
 		 	} else if(location.host.indexOf('neighborhood') >= 0) {
 		 		$(".siteTitleShort").text("Model Building");
-		 		$('#logoholder').html("<a href='/'><img style='height: 25px;margin: 30px 10px 4px 10px;' src='" + climbpath + "../localsite/img/logo/neighborhood.png' style='width:140px;padding-top:4px'></a>");
+		 		$('#logoholder').html("<a href='/'><img style='height: 25px;margin: 30px 10px 4px 10px;' src='" + climbpath + "../localsite/img/logo/favicon.png' style='width:140px;padding-top:4px'></a>");
 		 		$('.headerbar').css('height', '80px');
 		 		$('.headerOffsetOne').css('height', '80px');
 		 		$('.headerbarheight').css('height', '80px');
