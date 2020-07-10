c72f3362c9b5b223129315de17b52e7877e591c4
nazanin-tabatabaei <nazanin.tbt@gatech.edu>
2020-07-10 11:05:07 -0400

    finally fixed the dumb tooltip text

--- info/js/bubble.js
+++ info/js/bubble.js
@@ -236,7 +236,9 @@ function updateChart(x,y,z){
   //Fetch data
   var records = getDimensions(x,y,z);
   updateTitle(x,y,z);
-
+  x1=x;
+  y1=y;
+  z1=z;
   (records.y).sort(function(a,b){return a-b});
   var l = (records.y).length;
   var low = Math.round(l * 0.010);
@@ -287,7 +289,7 @@ function updateChart(x,y,z){
         div.transition()
           .duration(200)
           .style("opacity", .9);               
-          div.html("<b style='font-size:1.3em'>" + d.industry_detail + "</b><br/> " +x+": "+d.x+ "</b><br/> " +y+": "+ d.y + "</b><br/> " +z+": "+ d.z)
+          div.html("<b style='font-size:1.3em'>" + d.industry_detail + "</b><br/> " +x1+": "+d.x+ "</b><br/> " +y1+": "+ d.y + "</b><br/> " +z1+": "+ d.z)
             .style("left", (d3.event.pageX) + "px")
             .style("top", (d3.event.pageY - 28) + "px");                     
       })
