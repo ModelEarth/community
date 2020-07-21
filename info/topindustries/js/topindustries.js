314d475c42184f142fb9703bfeaecaa4d8d3f692
nazanin-tabatabaei <nazanin.tbt@gatech.edu>
2020-07-21 16:26:43 -0400

    bubble dropdown update by hash

--- info/topindustries/js/topindustries.js
+++ info/topindustries/js/topindustries.js
@@ -971,10 +971,7 @@ function topRatesInFips(dataSet, dataNames, fips, howMany, params){
 
                         updateHash({"naics":naicshash});
                         params = loadParams(location.search,location.hash);
-                        midFunc(d3.select("#graph-picklist-x").node().value,
-                            d3.select("#graph-picklist-y").node().value,
-                            d3.select("#graph-picklist-z").node().value,
-                            params);
+                        midFunc(params.x,params.y,params.z,params);
                         })
                 })
                 d3.csv(root + "data/county_ID_list.csv").then( function(consdata) {
