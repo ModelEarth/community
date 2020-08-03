afd9828a7c014d5f9e8f8e3291401092648148be
Elon Cha <54544200+elonncha@users.noreply.github.com>
2020-08-02 21:45:57 -0400

    folder rearrangement, add more examples

---
+++ start/charts/input-output-sk/example/data_energyjson.js
@@ -0,0 +1,32 @@
+// sample from: https://bl.ocks.org/mbostock/ca9a0bb7ba204d12974bca90acc507c0
+var energyjson = {"nodes":[
+{"name":"Industry A"},
+{"name":"Industry B"},
+{"name":"Industry C"},
+{"name":"Input 1"},
+{"name":"Input 2"},
+{"name":"Input 3"},
+{"name":"Output 1"},
+{"name":"Output 2"},
+{"name":"Output 3"}
+],
+"links":[
+{"source":3,"target":0,"value":124.729},
+{"source":3,"target":1,"value":124.729},
+{"source":3,"target":2,"value":124.729},
+{"source":4,"target":0,"value":124.729},
+{"source":4,"target":1,"value":124.729},
+{"source":4,"target":2,"value":124.729},
+{"source":5,"target":0,"value":124.729},
+{"source":5,"target":1,"value":124.729},
+{"source":5,"target":2,"value":124.729},
+{"source":0,"target":6,"value":124.729},
+{"source":0,"target":7,"value":124.729},
+{"source":0,"target":8,"value":124.729},
+{"source":1,"target":6,"value":124.729},
+{"source":1,"target":7,"value":124.729},
+{"source":1,"target":8,"value":124.729},
+{"source":2,"target":6,"value":124.729},
+{"source":2,"target":7,"value":124.729},
+{"source":2,"target":8,"value":124.729}
+]};
